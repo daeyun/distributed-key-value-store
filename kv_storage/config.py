0468947c5c46e2aefd74b6aba128b76399c09468
Daeyun Shin <daeyun@daeyunshin.com>
2014-04-29 17:36:19 -0500

    Create config file

---
+++ kv_storage/config.py
@@ -0,0 +1,14 @@
+"""
+hosts is a list of (IP address, port1, port2)
+
+ordering can be either 'casual' or 'total'
+"""
+
+config = {
+    'hosts': [
+        ('localhost', 16400, 16500),
+        ('localhost', 16401, 16501),
+        ('localhost', 16402, 16502),
+        ('localhost', 16403, 16503),
+    ],
+}
